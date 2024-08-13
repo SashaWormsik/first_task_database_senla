cee266c4b9affe64719316012e81932bd6a53675
Sasha <sashawormsik@gmail.com>
2024-08-13 03:12:16 +0300

    Add yaml. Error

--- main/liquibaseResources/changelogs/homework-2/sql/05.create-user_info-table.sql
+++ main/liquibaseResources/changelogs/homework-2/sql/05.create-user_info-table.sql
@@ -2,7 +2,7 @@ CREATE TABLE user_info
 (
     id              bigserial PRIMARY KEY,
     name            character varying(100),
-    surename        character varying(100),
+    surname        character varying(100),
     profession      character varying(100),
     work_experience integer,
     description     character varying(500)
