51f4993d6e7112b571f54d1508d7aa9143475ce3
Sasha <sashawormsik@gmail.com>
2024-09-08 17:08:30 +0300

    homework 5

--- src/main/java/org/charviakouski/freelanceExchange/config/JavaConfig.java
+++ src/main/java/org/charviakouski/freelanceExchange/config/JavaConfig.java
@@ -1,5 +1,6 @@
 package org.charviakouski.freelanceExchange.config;
 
+import liquibase.integration.spring.SpringLiquibase;
 import org.charviakouski.freelanceExchange.MyApplication;
 import org.charviakouski.freelanceExchange.model.mapper.EntityMapper;
 import org.springframework.context.annotation.Bean;
@@ -13,5 +14,12 @@ public class JavaConfig {
     @Bean
     public EntityMapper entityMapper() {
         return new EntityMapper();
+    }
+
+    @Bean
+    public SpringLiquibase liquibase() {
+        SpringLiquibase liquibase = new SpringLiquibase();
+        liquibase.setChangeLog("classpath:config/liquibase/master.xml");
+        return liquibase;
     }
 }
